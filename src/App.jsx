28bd7556a233982b9f757ac78397c2b91c4b03d8
YasserToni <altonyyasser@gmail.com>
2024-11-30 21:49:25 +0200

    update rouet

--- src/App.jsx
+++ src/App.jsx
@@ -33,11 +33,11 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="product-list" element={<ProductListPage />} />
-          <Route path="product-detials" element={<ProductPage />} />
-          <Route path="SuccessPayment" element={<SuccessPage />} />
-          <Route path="FaildPayment" element={<FaildPayment />} />
-          <Route path="order" element={<OrderPage />} />
+          <Route path="/product-list" element={<ProductListPage />} />
+          <Route path="/product-detials" element={<ProductPage />} />
+          <Route path="/SuccessPayment" element={<SuccessPage />} />
+          <Route path="/FaildPayment" element={<FaildPayment />} />
+          <Route path="/order" element={<OrderPage />} />
         </Routes>
       </BrowserRouter>
     </>
