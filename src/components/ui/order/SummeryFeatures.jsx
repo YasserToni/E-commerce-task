8d5d88d31a811c5f4490824ab5c0d95262ec3972
YasserToni <altonyyasser@gmail.com>
2024-12-04 16:53:57 +0200

    resolve special images

--- src/components/ui/order/SummeryFeatures.jsx
+++ src/components/ui/order/SummeryFeatures.jsx
@@ -1,43 +1,63 @@
 import { TfiHeadphoneAlt } from "react-icons/tfi";
-import { TbTruckDelivery } from "react-icons/tb";
+// import { TbTruckDelivery } from "react-icons/tb";
 import { MdOutlineDiscount } from "react-icons/md";
 import { FaMoneyCheckAlt } from "react-icons/fa";
+import deliveryIcon from "../../../assets/summery-Features-icon/delivery.png";
+import moneyIcon from "../../../assets/summery-Features-icon/money.png";
+import couponIcon from "../../../assets/summery-Features-icon/coupon.png";
+import headphoneIcon from "../../../assets/summery-Features-icon/headphone.png";
 
 function SummeryFeatures() {
   return (
     <div
       dir="rtl"
-      className="flex gap-16 sm:flex-wrap sm:items-center sm:justify-center"
+      className="flex gap-12 sm:gap-8 sm:flex-wrap sm:items-center sm:justify-between"
     >
-      <div className="flex flex-col items-center justify-center gap-3">
-        <div className="w-16 h-14 rounded-full bg-[#EAF1FD] text-primary flex items-center justify-center">
-          <TbTruckDelivery size={32} />
+      <div className="flex sm:w-[103px] sm:h-[81px] flex-col items-center justify-center gap-3">
+        <div className="w-16 h-16 sm:w-[53px] sm:h-[53px] rounded-[50%] bg-[#EAF1FD] text-primary pt-2 flex items-center justify-center">
+          <img
+            src={deliveryIcon}
+            alt="deliver icon"
+            className="w-[43px] h-[43px]"
+          />
         </div>
-        <div className="font-bold font-fb text-lg text-[#454545]">
+        <div className=" font-bold font-fb text-lg sm:text-sm text-[#454545]">
           توصيل مجاني
         </div>
       </div>
-      <div className="flex flex-col items-center justify-center gap-3">
-        <div className="w-16 h-14 rounded-full bg-[#EAF1FD] text-primary flex items-center justify-center">
-          <MdOutlineDiscount size={32} />
+      <div className="flex flex-col sm:w-[103px] sm:h-[81px]  items-center justify-center gap-3">
+        <div className="w-16 h-16 sm:w-[53px] sm:h-[53px] rounded-[50%] bg-[#EAF1FD] text-primary flex items-center justify-center">
+          <img
+            src={couponIcon}
+            alt="coupon icon"
+            className="w-[33px] h-[33px]"
+          />
         </div>
-        <div className="font-bold font-fb text-lg text-[#454545]">
+        <div className="font-bold font-fb text-lg sm:text-sm text-[#454545]">
           عروض وخصومات
         </div>
       </div>
-      <div className="flex flex-col items-center justify-center gap-3">
-        <div className="w-16 h-14 rounded-full bg-[#EAF1FD] text-primary flex items-center justify-center">
-          <FaMoneyCheckAlt size={32} />
+      <div className="flex flex-col sm:w-[110px] sm:h-[81px]  items-center justify-center gap-3">
+        <div className="w-16 h-16 sm:w-[53px] sm:h-[53px] rounded-[50%] bg-[#EAF1FD] text-primary flex items-center justify-center">
+          <img
+            src={moneyIcon}
+            alt="Money icon"
+            className="w-[38px]  h-[38px] "
+          />
         </div>
-        <div className="font-bold font-fb text-lg text-[#454545]">
+        <div className="font-bold font-fb text-lg sm:text-sm text-[#454545]">
           الدفع عند الاستلام
         </div>
       </div>
-      <div className="flex flex-col items-center justify-center gap-3">
-        <div className="w-16 h-14 rounded-full bg-[#EAF1FD] text-primary flex items-center justify-center">
-          <TfiHeadphoneAlt size={32} />
+      <div className="flex flex-col sm:w-[103px] sm:h-[81px]  items-center justify-center gap-3">
+        <div className="w-16 h-16 sm:w-[53px] sm:h-[53px] rounded-[50%] bg-[#EAF1FD] text-primary flex items-center justify-center">
+          <img
+            src={headphoneIcon}
+            alt="headphone icon"
+            className="w-[39px] h-[39px]"
+          />
         </div>
-        <div className="font-bold font-fb text-lg text-[#454545]">
+        <div className="font-bold font-fb text-lg sm:text-sm text-[#454545]">
           خدمة العملاء
         </div>
       </div>
--- src/components/ui/special/SpecialCard.jsx
+++ src/components/ui/special/SpecialCard.jsx
@@ -1,6 +1,6 @@
 import { WiDirectionUpLeft } from "react-icons/wi";
-
-function SpecialCard({ title, discount, imgUrl, imgAlt }) {
+import specialCardImage from "../../../assets/special-images/headphone.png";
+function SpecialCard() {
   return (
     <div className="flex flex-col shrink-0 rounded-md bg-[#F6F6F6] p-5 w-80 sm:w-[190px] h-96 sm:h-[230px]">
       <h3 className="font-semibold text-3xl sm:text-lg sm:leading-6 font-fm text-[#212328]">
@@ -15,7 +15,7 @@ function SpecialCard({ title, discount, imgUrl, imgAlt }) {
       </a>
       <img
         className="w-fit h-fit sm:w-[138px] sm:h-[138px]  object-cover"
-        src="https://s3-alpha-sig.figma.com/img/21b2/ec0b/ab2d6bdfc734791d0cf6c9c98a500926?Expires=1733097600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=NVHJIguUvJLN5Sla94VOe5C2kybbwuxs4CiF5T89jGtUbHsDp5P8wjcurT9eEv4PEdzC~yePmCOWBuV81KxnoQhk13mkImSWlQGP8JT9ZDtIsONaT36U0yhLxoyvrwj0hU4kZqFP~73MEn-ZNnSqIpGM86GjD7ehMFxjeaMpv9exrBKvUz87bvFqcnfTodh7H5S0dV8Yo5MDy63~s9Z9h9a7py4eEy-52LJHvvL1Lz0gtaVlU2Y~eDhvHp5AS2fWe5NG-7KkrpEdorHV7gi5rYFAtW-NBgvs6FpFefzTh4kvK~bGxGf3CNCttzS1jcEknQntkRXzdXjaE9DzPw0bWg__"
+        src={specialCardImage}
         alt="Headphones"
       />
     </div>
