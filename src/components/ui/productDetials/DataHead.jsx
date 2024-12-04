05fa586dde08d03f4a5785d345beef2c5d609504
YasserToni <altonyyasser@gmail.com>
2024-12-05 00:41:00 +0200

    update header

--- src/components/ui/productDetials/DataHead.jsx
+++ src/components/ui/productDetials/DataHead.jsx
@@ -4,7 +4,7 @@ function DataHead({ title }) {
   return (
     <div
       dir="rtl"
-      className=" my-7 sm:my-4 max-w-[580px] flex justify-start items-center gap-2 rounded-lg font-bold text-xl sm:text-base font-fb text-[#454545] bg-[#3074F014] p-4 "
+      className=" my-7 sm:my-4 max-w-[580px] flex justify-start items-center gap-2 rounded-lg font-bold text-xl sm:text-sm font-fb text-[#454545] bg-[#3074F014] p-4 "
     >
       <IoFlash className="text-[#3074F0]" />
       {title}
