bbef3a36d88c3b7734c17ae1722be9e1a45cd8fc
YasserToni <altonyyasser@gmail.com>
2024-12-05 16:15:43 +0200

    update product list card

--- src/components/ui/ProductList/ProductList.jsx
+++ src/components/ui/ProductList/ProductList.jsx
@@ -1,5 +1,5 @@
 import ProductListCard from "./ProductListCard";
-import SkeletonCard from "../LoadingCard.jsx/SkeletonCard";
+// import SkeletonCard from "../LoadingCard.jsx/SkeletonCard";
 
 function ProductList() {
   return Array.from({ length: 20 }).map((item, index) => (
